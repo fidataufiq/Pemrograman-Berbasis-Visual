1e524ceec9f142806247e7bae2e2aab738ca3294
Fida Taufiq <89764956+fidataufiq@users.noreply.github.com>
2024-01-14 17:54:17 +0700

    Tugas Praktikum Modul 5

---
+++ TugasPraktikum5/src/Tugas1/Koneksi.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugas1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Koneksi {
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL      = "jdbc:mysql://localhost/toko";
+    private static final String USER        = "root";
+    private static final String PASS        = "";
+    
+    private Connection conn                 = null;
+    
+    public Koneksi(){
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Koneksi Berhasil");
+        } catch (Exception e) {
+            System.out.println("Koneksi GAGAL");
+        }
+    }
+    
+    public Connection getConnection(){
+        return conn;
+    }
+}
